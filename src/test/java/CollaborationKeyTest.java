8d3e52f167304278255c9c7974dbe8b05e59d1b3
evgenhvost@gmail.com <hvostenko1>
2017-12-31 11:17:38 +0200

    Adding collaboration key

---
+++ src/test/java/CollaborationKeyTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CollaborationKeyTest {
+    @Test
+    public void testSameKey() {
+        CollaborationKey c1 = new CollaborationKey("moshe", "david");
+        CollaborationKey c2 = new CollaborationKey("david", "moshe");
+
+        Assert.assertEquals(c1.getKey(), c2.getKey());
+    }
+
+    @Test
+    public void testDifferentKey() {
+        CollaborationKey c1 = new CollaborationKey("moshe", "david");
+        CollaborationKey c2 = new CollaborationKey("rami", "moshe");
+
+        Assert.assertNotEquals(c1.getKey(), c2.getKey());
+    }
+
+    @Test
+    public void testSimilarIdsDifferentKey() {
+        CollaborationKey c1 = new CollaborationKey("abc", "def");
+        CollaborationKey c2 = new CollaborationKey("ab", "cdef");
+
+        Assert.assertNotEquals(c1.getKey(), c2.getKey());
+
+        c1 = new CollaborationKey("abcd", "ef");
+        c2 = new CollaborationKey("cdef", "ab");
+
+        Assert.assertNotEquals(c1.getKey(), c2.getKey());
+    }
+}
